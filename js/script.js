d10ede9f44bd8a7c768259634b5a3b2586fd45b2
phillip shields <efe.in.the.mountains@gmail.com>
2017-07-22 00:54:20 +0800

    fixed selectors, still not functionable

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.carousel').slick({
+    slidesToShow: 1,
+    autoplay: True,
+    autoplaySpeed: 4000,
+    arrows: False,
+    pauseOnHover: False,
+    draggable: False
+
+  });
+});
